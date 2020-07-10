8e322ea4456d6cb75683feeeabf7ee178ca48b8b
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-07-10 13:13:59 -0300

    - Bouncycastle test

--- src/main/java/br/com/sumware/sslpoke/SSLPoke.java
+++ src/main/java/br/com/sumware/sslpoke/SSLPoke.java
@@ -1,8 +1,14 @@
 package br.com.sumware.sslpoke;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+
+import br.com.sumware.sslpoke.net.TSLSocketConnectionFactory;
+
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Establish a SSL connection to a host and port, writes a byte and prints the
@@ -12,26 +18,79 @@ import java.io.*;
 public class SSLPoke {
 	public static void main(String[] args) {
 		if (args.length != 2) {
-			System.out.println("Usage: " + SSLPoke.class.getName() + " <host> <port>");
+			System.out.println("Usage: " + SSLPoke.class.getName() + " <host> <port> --bc");
 			System.exit(1);
 		}
 		try {
-			SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-			SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
+			if (args[1].equals("--bc")) {
+				SSLPoke m = new SSLPoke();
+				System.out.println(m.usingBouncycastle(args[0]));
+			} else {
+				SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+				SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
 
-			InputStream in = sslsocket.getInputStream();
-			OutputStream out = sslsocket.getOutputStream();
+				InputStream in = sslsocket.getInputStream();
+				OutputStream out = sslsocket.getOutputStream();
 
-			// Write a test byte to get a reaction :)
-			out.write(1);
+				// Write a test byte to get a reaction :)
+				out.write(1);
 
-			while (in.available() > 0) {
-				System.out.print(in.read());
+				while (in.available() > 0) {
+					System.out.print(in.read());
+				}
+				System.out.println("Successfully connected");
 			}
-			System.out.println("Successfully connected");
-
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
 	}
+	
+	/**
+	 * 
+	 * @author Daniel Pereira 
+	 *
+	 */
+	public String usingBouncycastle(String fmtUrl) {
+
+		String body = null;
+
+		try {
+
+			URL url = new URL(fmtUrl);
+			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+
+			con.setSSLSocketFactory(new TSLSocketConnectionFactory());
+
+			StringBuilder sb = new StringBuilder();
+			try {
+				con.setRequestMethod("GET");
+				con.setDoOutput(true);
+				con.setInstanceFollowRedirects(false);
+
+				int status = con.getResponseCode();
+				if (status == HttpURLConnection.HTTP_MOVED_TEMP || status == HttpURLConnection.HTTP_MOVED_PERM) {
+					String location = con.getHeaderField("Location");
+					URL newUrl = new URL(location);
+					con = (HttpsURLConnection) newUrl.openConnection();
+				}
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				while ((inputLine = in.readLine()) != null) {
+					sb.append(inputLine + "\n\r");
+				}
+			} finally {
+				con.disconnect();
+			}
+
+			body = sb.toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return body;
+
+	}
+
 }
